6407757977cd7ed9b00ecafe1b251a743d130912
Irwing Naranjo <khalisser@gmail.com>
2022-05-28 00:00:06 -0400

    REFAACTOR layout page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,9 @@
-import styles from '../styles/Home.module.css'
-
-const TITLE:string = 'AppStore'
 
 export default function Home () {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          {TITLE}
-        </h1>
+    <div>
+      <main>
+        <p>Productos...</p>
       </main>
     </div>
   )
