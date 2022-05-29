187c2fd2bb089557bc2efa319cef3a436cfbf632
Irwing Naranjo <khalisser@gmail.com>
2022-05-29 11:33:15 -0400

    REFACTOR design Header component

--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,30 @@
+import Img from 'next/image'
+
+const logo = require('../public/icon.png')
+const cartIcon = require('../public/cart-shopping.svg')
+
 const Header = () => {
   return (
-    <div>
-      <h1>Header...</h1>
-    </div>
+    <header className="header">
+      <div className="header-container">
+        <div className="header-logo">
+          <Img
+            src={logo} alt="Cuponatric logo"
+            layout='responsive'
+          />
+        </div>
+        <a href="/cart">
+          <div className="header-cart">
+              <Img
+                src={cartIcon} alt="Cart icon"
+                width={24}
+                height={24}
+              />
+              <span className="count">3</span>
+          </div>
+        </a>
+      </div>
+    </header>
   )
 }
 
