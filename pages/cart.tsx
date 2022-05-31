47941ba7e9c131f274b4bcbda0b81eef0b465da7
Irwing Naranjo <khalisser@gmail.com>
2022-05-30 22:54:49 -0400

    ADD Formix

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 import Img from 'next/image'
+import { Formik } from 'formik'
 import { ProductsCartView } from '../components/ProductsCartView'
 import { numberToMoney } from '../utils/formats'
 
@@ -15,49 +16,106 @@ const CartDetail = () => {
     setTotal(total)
   }, [])
 
-  const handlePayment = (e) => {
-    e.preventDefault()
-    alert('Pago')
-  }
-
   return (
     <div>
       <h3>Carrito de compras</h3>
       <p>Ingrese sus datos para completar el pedido.</p>
-      <form className="form-cart form-inline">
-        <div className="form-inline" style={{ flex: 1 }}>
-          <div className="form-group" style={{ width: 250 }}>
-            <label htmlFor="name">{'Nombre'}</label>
-            <input type="text" className="form-control" id="name" placeholder={'Nombre'} />
+      <Formik initialValues={{ name: '', phone: '', email: '' }}
+        validate={values => {
+          const errors = {}
+          if (values.name.length < 2) {
+            errors.name = 'Debe tener al menos 2 caracteres'
+          }
+          if (values.phone.length < 8) {
+            errors.phone = 'Debe tener al menos 8 caracteres'
+          }
+          if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+            errors.email = 'Es obligatorio y debe ser válido'
+          }
+          return errors
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          alert('El pago se realizó correctamente')
+          localStorage.setItem('cart', JSON.stringify([]))
+          window.location.href = '/'
+          setSubmitting(false)
+        }}
+      >
+       {({
+         values,
+         errors,
+         touched,
+         handleChange,
+         handleBlur,
+         handleSubmit,
+         isSubmitting
+       }) => (
+         <form className="form-cart form-inline" onSubmit={handleSubmit}>
+           <div className="form-inline" style={{ flex: 1 }}>
+            <div className="form-group" style={{ width: 250 }}>
+              <label htmlFor="name">{'Nombre'}</label>
+              <input
+                id="name"
+                className="form-control"
+                placeholder={'Nombre'}
+                type="text"
+                name="name"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.name}
+              />
+              <span>{errors.name && touched.name && errors.name}</span>
+            </div>
+            <div className="form-group" style={{ width: 200 }}>
+              <label htmlFor="phone">{'Teléfono'}</label>
+              <input
+                id="phone"
+                className="form-control"
+                placeholder={'Teléfono'}
+                type="text"
+                name="phone"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.phone}
+              />
+              <span>{errors.phone && touched.phone && errors.phone}</span>
+            </div>
+            <div className="form-group" style={{ flex: 1, width: '100%' }}>
+              <label htmlFor="email">{'Email'}</label>
+              <input
+                id="email"
+                className="form-control"
+                placeholder={'Email'}
+                type="email"
+                name="email"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+              />
+              <span>{errors.email && touched.email && errors.email}</span>
+            </div>
+            <div className="form-group" style={{ width: '100%' }}>
+              <Link href="/" style={{ width: '140px' }}>
+                {'Volver a la tienda'}
+              </Link>
+            </div>
           </div>
-          <div className="form-group" style={{ width: 200 }}>
-            <label htmlFor="phone">{'Teléfono'}</label>
-            <input type="text" className="form-control" id="phone" placeholder={'Teléfono'} />
+          <div className="form-group-infopay">
+            <p style={{ fontSize: '14pt' }}>Total a pagar</p>
+            <h3 style={{ fontSize: '22pt' }}>{numberToMoney(total)}</h3>
+            <button type="submit" disabled={isSubmitting} className="form-cart-pay">
+              <Img
+                src={payIcon} alt="Pay icon"
+                style={{ marginBottom: '-15px', marginRight: '5px', color: '#fff' }}
+                width={30}
+                height={18}
+              />
+              Pagar
+            </button>
           </div>
-          <div className="form-group" style={{ flex: 1, width: '100%' }}>
-            <label htmlFor="mail">{'Email'}</label>
-            <input type="text" className="form-control" id="mail" placeholder={'Email'} />
-          </div>
-          <div className="form-group" style={{ width: '100%' }}>
-            <Link href="/" style={{ width: '140px' }}>
-              {'Volver a la tienda'}
-            </Link>
-          </div>
-        </div>
-        <div className="form-group-infopay">
-          <p style={{ fontSize: '14pt' }}>Total a pagar</p>
-          <h3 style={{ fontSize: '22pt' }}>{numberToMoney(total)}</h3>
-          <button className="form-cart-pay" onClick={handlePayment}>
-            <Img
-              src={payIcon} alt="Pay icon"
-              style={{ marginBottom: '-15px', marginRight: '5px', color: '#fff' }}
-              width={30}
-              height={18}
-            />
-            Pagar
-          </button>
-        </div>
-      </form>
+         </form>
+       )}
+     </Formik>
     </div>
   )
 }
